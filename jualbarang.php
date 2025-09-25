30b102295374eb7d59b99ecd38fef4b171d9aed0
nfrshraa <nfarasahera@gmail.com>
2025-09-25 11:37:02 +0700

    setup awal

---
+++ jualbarang.php
@@ -0,0 +1,9 @@
+<?php
+// Daftar nama barang & harga dipisah
+$namaBarang = ["Sabun", "Shampoo", "Parfum", "Sunscreen", "Bedak"];
+$hargaBarang = [3000, 5000, 50000, 350000, 15000];
+
+echo "<h1>--POLGAN MART--</h1>";
+
+
+?>
